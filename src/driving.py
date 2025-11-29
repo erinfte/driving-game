03a2863532d78a1f0448a97faf4f69724d6b5e4c
kymquat <erinfette01@gmail.com>
2025-11-29 16:33:47 -0600

    Added speed to the player object and added the player to the screen. Object can travel in any 4 directions that align with wasd. player updates in main.

--- src/driving.py
+++ src/driving.py
@@ -3,29 +3,30 @@ import random
 
 
 class Car():
-      def __init__(self, x, y, w, h, color):
+    def __init__(self, x, y, w, h, color):
         self.rect = pygame.Rect(x, y, w, h)
         self.color = color
 
-        def draw(self, surf):
-            pygame.draw.rect(surf, self.color, self.rect)
+    def draw(self, surf):
+        pygame.draw.rect(surf, self.color, self.rect)
 
-# class Player(Car):
+class Player(Car):
  
-    # def __init__(self):
-        #x =
-        #y =
+    def __init__(self, x, y, w, h, color, speed):
+        super().__init__(x, y, w, h, color)
+        self.speed = speed
     
-    # def update(self):
-        # keys = pygame.key.get_pressed()
-        # if keys[pygame.K_w]:
-            # self.rect.y -= self.speed
-        # if keys[pygame.K_s]:
-            # self.rect.y += self.speed
-        # if keys[pygame.K_a]:
-            # self.rect.x -= self.speed
-        # if keys[pygame.K_d]:
-            # self.rect.x += self.speed
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_d]:
+            self.rect.x += self.speed
+
 
 # class NPC(Car):
 
@@ -37,6 +38,8 @@ def main():
     pygame.init()
     pygame.display.set_caption("Twelve 'o Clock Wheelman") #Play on Midnight Motorist xd
     clock = pygame.time.Clock()
+    clock.tick(60)
+    player = Player(100, 100, 10, 10, (100, 100, 100), 1)
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
     running = True
@@ -45,9 +48,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
 
+        player.update()
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+
+        pygame.display.flip()
+        
     pygame.quit()
 
 
